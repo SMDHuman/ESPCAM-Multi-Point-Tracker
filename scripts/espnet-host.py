055b1f4cd46bd013d009750591cfb3777746fd20
SMDHuman <48795375+SMDHuman@users.noreply.github.com>
2025-03-27 17:44:10 +0300

    Enhance ESPNet host functionality with improved peer handling, added FPS tracking, and optimized USBNow initialization

--- scripts/espnet-host.py
+++ scripts/espnet-host.py
@@ -1,13 +1,15 @@
 from usb_now.scripts.usbnow import USBNow, MAC
 from enum_parser import enum_parser
-import os, sys
+import os, sys, time
 from time import sleep
 from struct import pack, unpack
 import threading
 import pygame as pg
+from multiprocessing import freeze_support
 
 def new_peer(mac: MAC):
     global peers_points_surf, peers
+    peers_fps.append(pg.time.Clock())
     peers.append(mac)
     peers_points_surf.append(pg.Surface((240, 176)))
 
@@ -49,6 +51,7 @@ def recv_cb(addr: bytes, data: bytes):
             x2, y2 = points[i+2], points[i+3]
             rect = pg.Rect(x1, y1, x2-x1, y2-y1)
             pg.draw.rect(peers_points_surf[_id], (255, 0, 0), rect)
+        peers_fps[_id].tick()
         #print(f"Received {points} points from {MAC(addr)}")
 
 # Parse the ESPNET enum from the header file
@@ -59,9 +62,11 @@ ESPNET = enums["ESPNET_PACKETS"]
 print(enums)
 
 # Initialize 
-peers: list[MAC] = []
 
-usbnow = USBNow("COM11")
+peers: list[MAC] = []
+peers_fps: list[pg.time.Clock] = []
+usbnow = USBNow("COM11", timeout=0.25)
+usbnow.receive_thread.start()
 usbnow.register_recv_cb(recv_cb)
 err = usbnow.init()
 if(err):
@@ -72,6 +77,8 @@ print("USBNow initialized")
 pg.init()
 win = pg.display.set_mode((240*2, 176*2))
 pg.display.set_caption("ESPNet Host")
+
+
 
 point_rects: list[pg.Rect]= []
 peers_points_surf: list[pg.Surface] = []
@@ -90,11 +97,18 @@ while running:
         pg.draw.rect(win, (255, 255, 255), pg.Rect(240*(i%2), 176*(i//2), 240, 176), 1)
 
     for peer in peers:
-        usbnow.send(peer, bytes([ESPNET["PACKET_REQ_LEDTOGGLE"]]))
+        start_time = time.time()
+        res = usbnow.send(peer, bytes([ESPNET["PACKET_REQ_LEDTOGGLE"]]))
+        if(res):
+            print(f"Error sending to {peer}: {res}")
+        #print(f"Time taken to send: {time.time()-start_time}")
         #usbnow.send(peer, bytes([ESPNET["PACKET_REQ_PING"]]))
         usbnow.send(peer, bytes([ESPNET["PACKET_REQ_POINTS"]]))
+        fps = peers_fps[peers.index(peer)].get_fps()
+        font_surf = pg.font.SysFont("Arial", 12).render(f"{fps:.2f} FPS", True, (255, 255, 255))
+        win.blit(font_surf, (240*(peers.index(peer)%2)+3, 176*(peers.index(peer)//2)+176-12-3))
     sleep(0.03)
-
+    
     pg.display.flip()
     win.fill(0)
 
